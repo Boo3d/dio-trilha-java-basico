2b6c59985b919e70df7a8d206b477960605deecf
Dyego Henrique <dyego_henrique-boo@hotmail.com>
2023-03-19 18:26:36 -0300

    Termino da quarta aula.

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -23,8 +23,20 @@ public class Operadores {
         System.out.println(concatenacao);
 
         concatenacao = "1" + (1 + 1 + 1);
-        
+
         System.out.println(concatenacao);
 
+        int numero = 5;
+
+        System.out.println(- numero);
+        System.out.println(numero);
+        
+        numero = - numero;
+        
+        System.out.println(numero);
+        
+        numero = numero * -1;
+
+        System.out.println(numero);
     }    
 }
